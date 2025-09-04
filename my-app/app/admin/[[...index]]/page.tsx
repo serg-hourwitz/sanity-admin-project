5e092ca3d1a629e089c135af0e9e616df1c76aeb
Serhiy Hourwitz <shmone800@gmail.com>
2025-09-04 18:44:45 +0300

    removed studio into my-app

--- my-app/app/admin/[[...index]]/page.tsx
+++ my-app/app/admin/[[...index]]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { NextStudio } from 'next-sanity/studio';
-import config from '../../../../studio/sanity.config';
+import config from '../../../studio/sanity.config';
 
 export const dynamic = 'force-dynamic'; // потрібне для роботи студії в App Router
 
