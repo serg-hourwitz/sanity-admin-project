e541a90e4ba480ec520259e90f59cdada7119f90
Serhiy Hourwitz <shmone800@gmail.com>
2025-09-05 13:19:28 +0300

    corrected code in Header.tsx

--- my-app/components/Header.tsx
+++ my-app/components/Header.tsx
@@ -2,7 +2,7 @@ interface HeaderProps {
   title: string;
 }
 
-export const Header: React.FC.<HeaderProps> = ({title}) => {
+export const Header: React.FC<HeaderProps> = ({title}) => {
   return (
     <header className="bg-blue-500 p-4">
       <h1 className="text-white text-3xl">{title}</h1>
